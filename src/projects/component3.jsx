ea01387d8314c935629d883b7519a52074c077be
ninokvakhadze <ninokvakhadze2@gmail.com>
2023-07-30 22:37:58 +0400

    add few projects

--- src/projects/component3.jsx
+++ src/projects/component3.jsx
@@ -1,23 +1,70 @@
 import "./component3.css";
+import todo from "../images/todo.PNG";
+import notifications from "../images/notifications.PNG";
+import interactiveRating from  "../images/interactive-rating.PNG";
+import tipCalculator from "../images/tipcalculator.PNG";
+import passwordGenerator from "../images/password.PNG";
+import calculatorImg from "../images/calculator.PNG";
+import priceRange from "../images/price-range.jpg"
 import advice from "../images/advice.jpg";
 
 function Projects() {
   return (
     <div className="projects">
       <Project
-        image={advice}
-        name="advice generator"
-        link="https://ninokvakhadze.github.io/advice-generator-app/"
-        link2 = "https://github.com/ninokvakhadze/advice-generator-app"
+        image={todo}
+        name="To Do app"
+        link="https://todo-app-beta-mauve-57.vercel.app"
+        link2 = "https://github.com/ninokvakhadze/Todo-app"
       />
-      <Project image={advice} name="advice generator" />
-      <Project image={advice} name="advice generator" />
-      <Project image={advice} name="advice generator" />
-      <Project image={advice} name="advice generator" />
-      <Project image={advice} name="advice generator" />
-      <Project image={advice} name="advice generator" />
-      <Project image={advice} name="advice generator" />
-      <Project image={advice} name="advice generator" />
+      <Project
+        image={notifications}
+        name="notifications page"
+        link = "https://notification-page-delta.vercel.app/?vercelToolbarCode=yIVArV9eJmdgbk-"
+        link2="https://github.com/ninokvakhadze/notification-page"
+      />
+      <Project
+        image={interactiveRating}
+        name="interactive rating"
+        link="https://ninokvakhadze.github.io/interactive_rating_component/"
+        link2 = "https://github.com/ninokvakhadze/interactive_rating_component"
+      />
+      <Project
+        image={tipCalculator}
+        name="Tip calculator"
+        link="https://ninokvakhadze.github.io/tip-calculator/"
+        link2 = "https://github.com/ninokvakhadze/tip-calculator"
+      />
+       <Project
+        image={passwordGenerator}
+        name="Password generator"
+        link="https://ninokvakhadze.github.io/password-generator-app/"
+        link2 = "https://github.com/ninokvakhadze/password-generator-app"
+      />
+       <Project
+        image={calculatorImg}
+        name="Calculator"
+        link="https://ninokvakhadze.github.io/calculator/"
+        link2 = "https://github.com/ninokvakhadze/calculator"
+      />
+         <Project
+        image={priceRange}
+        name="Interactive rating"
+        link="https://ninokvakhadze.github.io/interactive-pricing-component/"
+        link2 = "https://github.com/ninokvakhadze/interactive-pricing-component"
+      />
+       <Project
+        image={calculatorImg}
+        name="Calculator"
+        link="https://ninokvakhadze.github.io/calculator/"
+        link2 = "https://github.com/ninokvakhadze/calculator"
+      />
+         <Project
+        image={priceRange}
+        name="Interactive rating"
+        link="https://ninokvakhadze.github.io/interactive-pricing-component/"
+        link2 = "https://github.com/ninokvakhadze/interactive-pricing-component"
+      />
     </div>
   );
 }
