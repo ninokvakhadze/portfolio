2861f1e9ac3800821db301165e3107f77fef1000
ninokvakhadze <ninokvakhadze2@gmail.com>
2023-07-30 13:45:28 +0400

    add skills

--- src/skills/component2.jsx
+++ src/skills/component2.jsx
@@ -1,11 +1,26 @@
 import "./component2.css";
+import html from "../images/html5-logo-2.png";
+import css from "../images/css3_badge.png";
+import js from "../images/js-logo-png-5.png";
+import reactLogo from "../images/react-logo-1000-transparent-300x300.png";
 
-
-function Skills(){
-    return(
-        <div className="skills"></div>
-    )
+function Skills() {
+  return (
+    <div className="skills">
+      <div className="skill-div">
+        <img src={html} className="image" />
+      </div>
+      <div className="skill-div">
+        <img src={css} className="image" />
+      </div>
+      <div className="skill-div">
+        <img src={js} className="image" />
+      </div>
+      <div className="skill-div">
+        <img src={reactLogo} className="image" />
+      </div>
+    </div>
+  );
 }
 
-
-export default Skills
+export default Skills;
